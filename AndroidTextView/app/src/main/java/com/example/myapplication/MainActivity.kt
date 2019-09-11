2b7a0f7b0467c98c42ee5f3b151bc79e846e4789
Le Thi Thuy Van <van.le@student.passerellesnumeriques.org>
2019-09-11 10:17:06 +0700

    link textView to kotlin

--- AndroidTextView/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ AndroidTextView/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.myapplication
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var txtView = findViewById(R.id.txtView) as TextView
+        txtView.text = "Hello world---Ahihi"
+        txtView.setTextColor(Color.parseColor("#a4c639"));
+
     }
 }
